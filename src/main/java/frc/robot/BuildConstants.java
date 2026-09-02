78089ee03355f72f0efd606c571a0f682c76ae9c
jrrachat <john.rachwalski1@gmail.com>
2026-09-02 16:53:24 -0400

    changes for autroutines

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "G4-Rebuilt";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 26;
-  public static final String GIT_SHA = "5536abbbfd10e802f3094e394e11d34b469f6abf";
-  public static final String GIT_DATE = "2026-08-31 18:15:19 EDT";
+  public static final int GIT_REVISION = 29;
+  public static final String GIT_SHA = "ce005404c60545fae0b37fb9b9978cc2dfd8a100";
+  public static final String GIT_DATE = "2026-08-31 18:35:37 EDT";
   public static final String GIT_BRANCH = "august";
-  public static final String BUILD_DATE = "2026-08-31 18:30:35 EDT";
-  public static final long BUILD_UNIX_TIME = 1788215435272L;
+  public static final String BUILD_DATE = "2026-08-31 19:39:44 EDT";
+  public static final long BUILD_UNIX_TIME = 1788219584883L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
